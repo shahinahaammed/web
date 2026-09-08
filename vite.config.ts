0338543536d41bcdaf2b552e3953d5eda173315a
shahin <shahinahd3@gmail.com>
2026-09-08 15:20:22 +0400

    Disable React Compiler, fix theme token

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
+    react()
   ],
 })
